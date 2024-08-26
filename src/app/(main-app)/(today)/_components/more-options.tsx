f2c9392c1705669f6c1e2be61c31f0f9ffb2e87c
mnacfil <mnacfil@gmail.com>
2024-08-26 09:33:42 +0800

    add more option in popover

--- src/app/(main-app)/(today)/_components/more-options.tsx
+++ src/app/(main-app)/(today)/_components/more-options.tsx
@@ -2,9 +2,12 @@
 
 import React from "react";
 import {
+  AlarmClock,
+  Clock,
   CopyPlus,
   Edit,
   Ellipsis,
+  Flag,
   Grip,
   Link,
   List,
@@ -34,6 +37,13 @@ import {
 import { addTask, deleteTask, getCurrentUser } from "../action";
 import { usePathname } from "next/navigation";
 import { toast } from "@/components/ui/use-toast";
+import { dueDates, priorities } from "@/components/constants";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 type Props = {
   task: Task;
@@ -43,6 +53,9 @@ type Props = {
 
 const MoreOptions = ({ task, user, onClickEdit }: Props) => {
   const pathname = usePathname();
+
+  const activePriority = "P4";
+
   const handleDeleteTask = async () => {
     await deleteTask({ taskId: task.id, pathname });
   };
@@ -68,76 +81,131 @@ const MoreOptions = ({ task, user, onClickEdit }: Props) => {
     }
   };
 
+  const handleSetPriority = async (priority: string) => {};
+
+  const handleDueDate = async () => {};
+
   return (
     <Popover>
       <PopoverTrigger asChild>
         <Ellipsis size={16} className="text-gray-400 cursor-pointer" />
       </PopoverTrigger>
-      <PopoverContent className="sm:max-w-[250px]">
-        <div className="flex flex-col divide-y divide-slate-300 mt-5 gap-3 w-full">
-          <div className="flex flex-col gap-2">
-            <ActionRow
-              Icon={Edit}
-              title="Edit"
-              endInfo="Ctrl E"
-              onClick={onClickEdit}
-            />
-            <ActionRow
-              Icon={List}
-              title="Go to project"
-              EndInfoIcon={MoveUp}
-              endInfo="G"
-            />
-          </div>
-          <div className="flex flex-col gap-1">
-            <ActionRow Icon={Grip} title="Move to..." endInfo="V" />
-            <ActionRow
-              Icon={CopyPlus}
-              title="Duplicate"
-              onClick={handleDuplicateTask}
-            />
-            <ActionRow
-              Icon={Link}
-              title="Copy link to task"
-              endInfo="Ctrl C"
-              EndInfoIcon={MoveUp}
-            />
-          </div>
-          <div className="flex flex-col gap-2">
-            <AlertDialog>
-              <AlertDialogTrigger asChild>
-                <ActionRow
-                  Icon={Trash}
-                  title="Delete"
-                  EndInfoIcon={MoveUp}
-                  endInfo="Delete"
-                  color="text-red-500"
-                />
-              </AlertDialogTrigger>
-              <AlertDialogContent>
-                <AlertDialogHeader>
-                  <AlertDialogTitle className="text-sm">
-                    Are you sure you want to delete{" "}
-                    <strong>{task.title}</strong>?
-                  </AlertDialogTitle>
-                  <AlertDialogDescription className="text-xs">
-                    This action cannot be undone. This will permanently delete
-                    your task and remove your task from our database.
-                  </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                  <AlertDialogCancel className="text-sm bg-gray-100 border-none hover:bg-gray-200 text-[12px] px-3">
-                    Cancel
-                  </AlertDialogCancel>
-                  <AlertDialogAction
-                    className="bg-red-600/85 text-white hover:bg-red-600 text-[12px] px-3"
-                    onClick={handleDeleteTask}
-                  >
-                    Continue
-                  </AlertDialogAction>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
+      <PopoverContent className="sm:max-w-[250px] p-0">
+        <div className="mt-5 p-1 w-full">
+          <div className="flex flex-col divide-y divide-slate-300 gap-1">
+            <div className="flex flex-col gap-1">
+              <ActionRow
+                Icon={Edit}
+                title="Edit"
+                endInfo="Ctrl E"
+                onClick={onClickEdit}
+              />
+              <ActionRow
+                Icon={List}
+                title="Go to project"
+                EndInfoIcon={MoveUp}
+                endInfo="G"
+              />
+            </div>
+            <div className="flex flex-col pt-1">
+              <ActionRow title="Due date" isLabel={true} endInfo="T">
+                <div className="flex space-x-4">
+                  {dueDates.map((dueDate) => (
+                    <TooltipProvider key={dueDate.value}>
+                      <Tooltip>
+                        <TooltipTrigger>
+                          <dueDate.Icon />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>{dueDate.value}</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  ))}
+                </div>
+              </ActionRow>
+              <ActionRow title="Priority" isLabel={true} endInfo="Y">
+                <div className="flex items-center space-x-4">
+                  {priorities.map((priority) => (
+                    <TooltipProvider key={priority.label}>
+                      <Tooltip>
+                        <TooltipTrigger asChild>
+                          <div
+                            className={`p-1 rounded-sm hover:bg-gray-100 cursor-pointer ${
+                              priority.label === activePriority
+                                ? "border-b ring-1 ring-gray-200"
+                                : "border-none"
+                            }`}
+                          >
+                            <Flag
+                              size={20}
+                              color={priority.color}
+                              onClick={() => handleSetPriority(priority.label)}
+                            />
+                          </div>
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>{priority.value}</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  ))}
+                </div>
+              </ActionRow>
+            </div>
+            <div className="flex flex-col pt-1">
+              <ActionRow Icon={AlarmClock} title="Reminders" />
+            </div>
+            <div className="flex flex-col gap-1">
+              <ActionRow Icon={Grip} title="Move to..." endInfo="V" />
+              <ActionRow
+                Icon={CopyPlus}
+                title="Duplicate"
+                onClick={handleDuplicateTask}
+              />
+              <ActionRow
+                Icon={Link}
+                title="Copy link to task"
+                endInfo="Ctrl C"
+                EndInfoIcon={MoveUp}
+              />
+            </div>
+            <div className="flex flex-col gap-2 py-3">
+              <AlertDialog>
+                <AlertDialogTrigger asChild>
+                  <ActionRow
+                    Icon={Trash}
+                    title="Delete"
+                    EndInfoIcon={MoveUp}
+                    endInfo="Delete"
+                    color="text-red-500"
+                  />
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle className="text-sm">
+                      Are you sure you want to delete{" "}
+                      <strong>{task.title}</strong>?
+                    </AlertDialogTitle>
+                    <AlertDialogDescription className="text-xs">
+                      This action cannot be undone. This will permanently delete
+                      your task and remove your task from our database.
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel className="text-sm bg-gray-100 border-none hover:bg-gray-200 text-[12px] px-3">
+                      Cancel
+                    </AlertDialogCancel>
+                    <AlertDialogAction
+                      className="bg-red-600/85 text-white hover:bg-red-600 text-[12px] px-3"
+                      onClick={handleDeleteTask}
+                    >
+                      Continue
+                    </AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
+            </div>
           </div>
         </div>
       </PopoverContent>
--- src/components/global/action-row.tsx
+++ src/components/global/action-row.tsx
@@ -1,15 +1,17 @@
 import { Edit, List, LucideProps, MoveUp } from "lucide-react";
-import React from "react";
+import React, { ReactNode } from "react";
 
 type Props = {
   title: string;
+  isLabel?: boolean;
   endInfo?: string;
-  Icon: React.ForwardRefExoticComponent<
+  Icon?: React.ForwardRefExoticComponent<
     Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>
   >;
   TitleIcon?: Props["Icon"];
   EndInfoIcon?: Props["Icon"];
   color?: string;
+  children?: ReactNode;
   onClick?: () => void;
 };
 
@@ -20,25 +22,38 @@ const ActionRow = ({
   EndInfoIcon,
   TitleIcon,
   color,
+  isLabel,
+  children,
   onClick,
 }: Props) => {
   return (
-    <div
-      className="flex gap-2 items-center justify-between p-1 rounded-md hover:bg-gray-100 cursor-pointer"
-      onClick={onClick}
-    >
-      <div className="flex gap-2 items-center">
-        <Icon size={14} className={`${color ? color : "text-gray-400"}`} />
-        <h6 className={`text-sm font-light ${color}`}>{title}</h6>
-        {TitleIcon && <TitleIcon size={12} className="text-gray-400" />}
+    <>
+      <div
+        className="flex gap-2 items-center justify-between p-1 rounded-md hover:bg-gray-100 cursor-pointer"
+        onClick={onClick}
+      >
+        <div className="flex gap-2 items-center">
+          {Icon && (
+            <Icon size={14} className={`${color ? color : "text-gray-400"}`} />
+          )}
+          <h6
+            className={`text-sm ${color} ${
+              isLabel ? "font-[500]" : "font-light"
+            }`}
+          >
+            {title}
+          </h6>
+          {TitleIcon && <TitleIcon size={12} className="text-gray-400" />}
+        </div>
+        <div className="flex items-center justify-between">
+          {EndInfoIcon && <EndInfoIcon size={12} className="text-gray-400" />}
+          {endInfo && (
+            <p className={`text-[11px] text-gray-700 font-light`}>{endInfo}</p>
+          )}
+        </div>
       </div>
-      <div className="flex items-center justify-between">
-        {EndInfoIcon && <EndInfoIcon size={12} className="text-gray-400" />}
-        {endInfo && (
-          <p className={`text-[11px] text-gray-700 font-light`}>{endInfo}</p>
-        )}
-      </div>
-    </div>
+      <div className="p-1">{children}</div>
+    </>
   );
 };
 

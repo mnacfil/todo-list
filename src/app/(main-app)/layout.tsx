4a6d2443aed57938161b11de8eda3cb16c485fd1
mnacfil <mnacfil@gmail.com>
2024-08-30 21:19:42 +0800

    create user drop-down

--- src/app/(main-app)/layout.tsx
+++ src/app/(main-app)/layout.tsx
@@ -8,7 +8,7 @@ type Props = {
 const layout = ({ children }: Props) => {
   return (
     <main className="h-screen w-full flex">
-      <div className="w-[300px] h-screen bg-slate-400 hidden lg:block">
+      <div className="w-[300px] h-screen bg-orange-50/50 hidden lg:block p-4">
         <Sidebar />
       </div>
       <div className="flex-1 mx-auto max-w-screen-lg h-full py-10 px-14">
--- src/components/layout/sidebar/index.tsx
+++ src/components/layout/sidebar/index.tsx
@@ -1,9 +1,39 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { PanelRight } from "lucide-react";
 import React from "react";
+import { UserDropdown } from "./user-dropdown";
 
 type Props = {};
 
 const Sidebar = (props: Props) => {
-  return <div>Sidebar</div>;
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <UserDropdown />
+        <div>
+          <Sheet>
+            <SheetTrigger asChild>
+              <PanelRight className="w-4 h-4 opacity-50 cursor-pointer" />
+            </SheetTrigger>
+            <SheetContent className="w-[400px] sm:w-[300px]" side="left">
+              <SheetHeader>
+                <SheetTitle>My Name</SheetTitle>
+              </SheetHeader>
+              <div>More Content</div>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Sidebar;
